41cb3e707f5cf06755869c64624003c74138e024
TonyLaPoche <terrade.antoine.pro@gmail.com>
2022-10-30 23:24:47 +0100

    importation repos git , etape du tuto 'authentification et sécurité'

--- ng-pokemon-app/src/app/app.component.ts
+++ ng-pokemon-app/src/app/app.component.ts
@@ -1,20 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1> Liste de Pokémons </h1>`
+  templateUrl: 'app.component.html'
 })
-export class AppComponent implements OnInit{
-  pokemonList: Pokemon[] = POKEMONS;
-
-  ngOnInit() {
-    console.table(this.pokemonList);
-    this.selectPokemon(this.pokemonList[2]);
-  }
-
-  selectPokemon(pokemon: Pokemon) {
-    console.log(`vous avez cliquer sur le pokémon ${pokemon.name}`);
-  }
-}
+export class AppComponent {}
